6aed9c2eb3db1b490d8dfc0c74b56866fd8892d7
DanikingRD <danikingrd@gmail.com>
2022-02-15 20:24:15 -0400

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:digital_card_app/screen/auth_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main(List<String> args) {
+  runApp(HomePageWidget());
+}
+
+class HomePageWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AuthScreen(),
+    );
+  }
+}
+
